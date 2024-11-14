6288dfe09b7eefe5cdfdde57b2dbcf750716fcdc
Tarun Gupta <tarun234.tg@gmail.com>
2024-11-14 21:10:59 +0530

    trial1

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -2,6 +2,7 @@ function App() {
 
   return (
     <>
+      {/* Delete this and start your project */}
       <div className="bg-[#0d0d0d] w-dvh h-dvh flex justify-center items-center">
         <div className="text-white text-8xl">Revo</div>
       </div>
