079b0625a485605d06fb24ac39b1a0a8fb55fc81
Tarun Gupta <tarun234.tg@gmail.com>
2024-11-21 18:03:26 +0530

    converted png to svg

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -1,6 +1,5 @@
-import revo from './assets/revo.png';
-import revo2 from './assets/revo2.png';
-import texture from './assets/texture.jpg';
+import revo from './assets/revo.svg';
+import revo2 from './assets/revo2.svg';
 // import {motion} from 'framer-motion';
 
 function App() {
@@ -12,7 +11,6 @@ function App() {
       <div className="bg-[#000000] w-dvw h-dvh flex justify-center items-center select-none">
         <img src={revo2} alt="Revo" className='absolute w-[30%] max-w-[200px] -mt-20 z-10' />
         <img src={revo} alt="Revo" className='absolute w-[60%] max-w-[400px] -mb-12 md:-mb-20 z-20' />
-        <img src={texture} alt='texture' className='absolute w-dvw h-dvh opacity-10 z-30'></img>
         <div className='bg-transparent w-dvw h-dvh absolute z-50'></div>
       </div>
     </>
